1b6da5e0cf063a9e8e5d02f2cefc595e25c26bb5
SHASHI9705 <singhshashiranjan34@gmail.com>
2025-01-30 19:12:22 +0530

    com

--- index.js
+++ index.js
@@ -6,24 +6,19 @@ const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
-// Middleware
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: true }));
 
-// Database Connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("MongoDB Connected"))
     .catch(err => console.log(err));
 
-// Root Route - Welcome Page
 app.get('/', (req, res) => {
     res.render('welcome');
 });
 
-// Authentication Routes
 app.use('/auth', authRoutes);
 
-// Start Server
 app.listen(3000, () => console.log("Server running on port 3000"));
